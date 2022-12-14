88e423a4b2335bd01d0dfa31a9eef296fcc9246c
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-14 19:50:20 -0300

    fazendo Deploy

---
+++ src/components/button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./button.css";
+
+const Button = (props) => {
+  return (
+    <button
+      onClick={(e) => props.click && props.click(e.target.innerHTML)}
+      className={`
+        button
+        ${props.operation ? "operation" : ""}
+        ${props.double ? "double" : ""}
+        ${props.triple ? "triple" : ""}
+      `}
+    >
+      {props.label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/display.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './display.css';
+
+const Display = (props) => {
+  return (
+    <div className='display'>
+      {props.value}
+    </div>
+  );
+}
+
+export default Display;
